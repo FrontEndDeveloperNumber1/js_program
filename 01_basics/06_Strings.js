10b5e939177791f0460cf04a37bd30c3fcf1cd3c
FrontEndDeveloperNumber1 <68110575+FrontEndDeveloperNumber1@users.noreply.github.com>
2024-07-03 09:25:02 +0000

    strings

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,17 @@
+const name = "jignesh"
+const repoCount = 10
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("jigneshthedeveloper");
+
+console.log(gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+
+// chart, slice , trim(), split
