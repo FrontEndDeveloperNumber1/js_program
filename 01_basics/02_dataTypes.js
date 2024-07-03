38e699a3df9b172d1f2bc6c974d46828e60242f4
FrontEndDeveloperNumber1 <68110575+FrontEndDeveloperNumber1@users.noreply.github.com>
2024-07-03 06:32:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code an newer version
+
+// alert(3 + 3) We are using nodejs, not broweser 
+
+console.log(3 + 3); // code readability important 
+
+console.log("Jignesh");
+
+let name = "jig" // sting
+let age = 30 // number
+let isLoggedIn = false // boolan
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "jig");
+
+
+
+
+
+
+
+
+
+
+
